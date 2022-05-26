d45c26b14bade919c76e488d2d47eab37da81bae
Leptos <leptos.0.null@gmail.com>
2022-05-26 07:17:26 -0700

    [General] Add Multi-platform SwiftUI Xcode project

---
+++ Shared/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Shared
+//
+//  Created by Leptos on 5/26/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Shared/ZoneInfoApp.swift
@@ -0,0 +1,17 @@
+//
+//  ZoneInfoApp.swift
+//  Shared
+//
+//  Created by Leptos on 5/26/22.
+//
+
+import SwiftUI
+
+@main
+struct ZoneInfoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
